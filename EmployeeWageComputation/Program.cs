7bd38d84199463d4c48978b99ed7f4895a5a023c
Hemant1803 <hshemantsharma445@gmail.com>
2023-07-04 08:49:34 +0530

    EmployeeAttendence Randomly

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to EmployeeWageComputation!");
+            const int IS_PRESENT = 1;
+            Random random = new Random();
+            int employeeInput=random.Next(0, 2);
+            Console.WriteLine("Randome value:{0}", employeeInput);
+            if (employeeInput==IS_PRESENT) 
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }   
         }
     }
 }
